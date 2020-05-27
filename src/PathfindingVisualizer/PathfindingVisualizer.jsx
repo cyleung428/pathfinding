b0f59816c9d15da361bde41df8ae7fefe3cdab3d
cyleung428 <msf96a85@gmail.com>
2020-05-27 20:12:20 +0800

    fix size

--- src/PathfindingVisualizer/PathfindingVisualizer.jsx
+++ src/PathfindingVisualizer/PathfindingVisualizer.jsx
@@ -198,7 +198,7 @@ export default class PathfindingVisualizer extends Component {
     const grid = [];
     for (let row = 0; row < 30; row++) {
       const currentRow = [];
-      for (let col = 0; col < 50; col++) {
+      for (let col = 0; col < 75; col++) {
         currentRow.push(this.createNode(col, row));
       }
       grid.push(currentRow);
